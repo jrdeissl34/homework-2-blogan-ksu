c4a808f640ab3d67f5d007d91cf285a2568496e6
Justin Deissler <jrdeissl34@ksu.edu>
2019-09-18 23:20:19 -0500

    Modified temperature.sh to also convert to Kelvin

--- temperature.sh
+++ temperature.sh
@@ -4,5 +4,7 @@ echo 'Please enter a temperature in degrees Fahrenheit:'
 read fahr
 
 celc=`echo "scale=3;($fahr-32)*5/9" | bc`
+kelv=`echo "scale=3;$celc+273.15" | bc`
 
 echo "$fahr degrees Fahrenheight is $celc degrees Celcius"
+echo "$fahr degrees Fahrenheight is $kelv Kelvin"
